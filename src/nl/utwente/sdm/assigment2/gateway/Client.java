096d76037f0aa39dd6499db4f548f3e08066d272
onwieze <onwieze@f0cc3384-9f4d-11dd-91bf-632da8b6e10b>
2008-10-21 15:41:15 +0000

    Added a class for the Protocol Commands and changed the protocol class for this. Also added some more comment in the classes.

--- src/nl/utwente/sdm/assigment2/gateway/Client.java
+++ src/nl/utwente/sdm/assigment2/gateway/Client.java
@@ -1,55 +1,107 @@
 package nl.utwente.sdm.assigment2.gateway;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
+/**
+ * The client object, this represents a clients which is registered in the gateway.
+ * @author Harmen
+ */
 public class Client {
+	/** The identity of the client. */
 	private String _identity;
+	/** The collection with trapdoors. */
 	private HashMap<String, TrapdoorAction> _trapdoors;
-	private HashSet<Device> _devices;
-	private Device _defaultDevice;
+	/** The devices of the client. */
+	private HashMap<String, Device> _devices;
+	/** The address of the default device of the client. */
+	private String _defaultDevice;
 	
+	/**
+	 * Constructor.
+	 * @param identity The identity of the client.
+	 * @param defaultDeviceAddress The address of the default client.
+	 */
 	public Client(String identity, String defaultDeviceAddress) {
 		_identity = identity;
 		_trapdoors = new HashMap<String, TrapdoorAction>();
-		_devices = new HashSet<Device>();
-		_defaultDevice = new Device(defaultDeviceAddress);
+		_devices = new HashMap<String, Device>();
+		_defaultDevice = defaultDeviceAddress;
+		addDevice(_defaultDevice);
 	}
 	
+	/**
+	 * Procedure to add a trapdoor for the client.
+	 * @param keywordHash The hashed keyword.
+	 * @param trapdoorAction The TrapdoorAction corresponding to the keyword.
+	 */
 	public void addTrapdoor(String keywordHash, TrapdoorAction trapdoorAction) {
 		_trapdoors.put(keywordHash, trapdoorAction);
 	}
 	
+	/**
+	 * Check whether there exists a trapdoor for a hashed keyword.
+	 * @param keyword The hashed keyword.
+	 * @return True when a trapdoor exists, otherwise false.
+	 */
 	public boolean trapdoorForKeywordExist(String keyword) {
 		return _trapdoors.containsKey(keyword);
 	}
 	
+	/**
+	 * Get the trapdoor for a certain hashed keyword.
+	 * @param keyword The hashed keyword.
+	 * @return The TrapdoorAction for the keyword.
+	 */
 	public TrapdoorAction getTrapdoorForKeyword(String keyword) {
 		return _trapdoors.get(keyword);
 	}
 	
-	public void addDevice(Device device) {
-		_devices.add(device);
+	/**
+	 * Add a device for the client.
+	 * @param device The device to add.
+	 */
+	public void addDevice(String address) {
+		_devices.put(address, new Device(address));
 	}
 	
+	/**
+	 * Remove a trapdoor for this client/
+	 * @param keywordHash The hashed keyword of the trapdoor to remove.
+	 */
 	public void removeTrapdoor(String keywordHash) {
 		if (_trapdoors.containsKey(keywordHash))
 			_trapdoors.remove(keywordHash);
 	}
 	
+	/**
+	 * Remove a device for this client.
+	 * @param device The device to remove.
+	 */
 	public void removeDevice(String device) {
-		if (_devices.contains(device))
+		if (_devices.containsKey(device))
 			_devices.remove(device);
 	}
 	
+	/**
+	 * Procedure to set the default device.
+	 * @param defaultDeviceAddress
+	 */
 	public void setDefaultDevice(String defaultDeviceAddress) {
-		_defaultDevice = new Device(defaultDeviceAddress);
+		_defaultDevice = defaultDeviceAddress;
 	}
 	
+	/**
+	 * Get the default device, of which the address is _defaultDevice.
+	 * @return The default device.
+	 */
 	public Device getDefaultDevice() {
-		return _defaultDevice;
+		return _devices.get(_defaultDevice);
 	}
 	
+	/**
+	 * Get the identity of the gateway.
+	 * @return The identity of the gateway.
+	 */
 	public String getIdentity() {
 		return _identity;
 	}
--- src/nl/utwente/sdm/assigment2/gateway/GatewayServerThread.java
+++ src/nl/utwente/sdm/assigment2/gateway/GatewayServerThread.java
@@ -3,38 +3,56 @@ package nl.utwente.sdm.assigment2.gateway;
 import java.net.*;
 import java.io.*;
 
+/**
+ * The Server thread for the gateway, these threads are created for each client which connects to the gateway.
+ * @author Harmen
+ */
 public class GatewayServerThread extends Thread {
+	/** The maximal length of a message, which is received. */
+	private static final int MAX_MESSAGE_LENGTH = 1000000;
+	
+	/** The socket of the server. */
 	private Socket socket = null;
 
+	/**
+	 * Constructor.
+	 * @param socket The socket for the server thread.
+	 */
 	public GatewayServerThread(Socket socket) {
 		super("GatewayServerThread");
 		this.socket = socket;
 	}
 
+	/**
+	 * @see java.lang.Thread#run()
+	 */
 	public void run() {
 		try {
+			// Create the input and output stream.
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+			// Read in the whole message. The maximum length of the message is MAX_MESSAGE_LENGTH.
 			StringBuffer input = new StringBuffer();
 			String inputLine;
 			while ((inputLine = in.readLine()) != null) {
-				if (input.length() < 1000000) {
+				if (input.length() < MAX_MESSAGE_LENGTH) {
 					input.append(inputLine);
 				} else {
 					out.write("Message too long!");
-					System.out.println("Message too long.");
+					System.out.println("Received message too long!");
 					break;
 				}
 			}
 			
+			// Now delegate the message to the protocol, which will process it and get the correct response.
 			IBEMessageProtocol imp = new IBEMessageProtocol();
 			out.write(imp.processInput(input.toString()));
 			
+			// Close the streams.
 			out.close();
 			in.close();
 			socket.close();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
