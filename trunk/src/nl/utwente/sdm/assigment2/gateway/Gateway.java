aa4dc655a104e9ad1b4b7ca99779f39adc6b4acd
onwieze <onwieze@f0cc3384-9f4d-11dd-91bf-632da8b6e10b>
2008-10-21 09:33:17 +0000

    First setup for the project.

---
+++ trunk/src/nl/utwente/sdm/assigment2/gateway/Gateway.java
@@ -0,0 +1,16 @@
+package nl.utwente.sdm.assigment2.gateway;
+
+/**
+ * The Main class for the gateway.
+ * @author Harmen
+ */
+public class Gateway {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Starting the gateway...");
+	}
+
+}
